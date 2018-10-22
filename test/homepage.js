0a04ffd60a6d3ce009958809d42b42c8c568c3b5
JCornflower <31407200+JCornflower@users.noreply.github.com>
2018-10-22 16:43:44 +0300

    result

--- test/homepage.js
+++ test/homepage.js
@@ -1,9 +1,40 @@
 var assert = require('assert');
-
+var request = require('request');
 describe('webdriver.io page', function(){
+//    // before ( () =>{
+//         browser.init();
+
+
+//         browser.end();
+//     })
     it('should have the right title', () =>{
         browser.url('/');
         var title = browser.getTitle();
-        assert.equal(title, 'WebdriverIO - Selenium 2.0 javascript bindings for nodejs');
+        assert.equal(title, 'WebdriverIO - WebDriver bindings for Node.js');
+    });
+
+    describe('Api page',()=>{
+        it('should have a link to the API page', () =>{
+            browser.url('/');
+            var hasApiLink = browser.isExisting('=API')
+            //browser.isExisting('a[href = "/api.html"]')
+
+            assert(hasApiLink);
+        });
+
+        it('should take an api page title', () =>{
+            browser.url('/');
+            browser.click('=API');
+
+            var title = browser.getTitle();
+            assert.equal(title, 'WebdriverIO - API Docs');
+        });
+
+        it('should filter search results', () =>{
+            browser.url('/api.html');
+            browser.setValue('input[name="search"]','debug');
+            browser.saveScreenshot('api-with-result.png');
+            //request.post("vrt-service.com", {});
+        });
     });
 });
--- wdio.conf.js
+++ wdio.conf.js
@@ -131,6 +131,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd'
+       // timeout: 99999999
     },
     //
     // =====
